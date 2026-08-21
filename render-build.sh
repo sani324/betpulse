1d44112d566da0b1e3e38917052451b472d436f7
sani324 <justicesanibashir@gmail.com>
2026-08-21 17:25:13 +0500

    Fix vite not found by setting pnpm install --prod=false

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 echo "Starting Render Build..."
-npx pnpm@9 install --no-frozen-lockfile
+export NODE_ENV=development
+npx pnpm@9 install --prod=false --no-frozen-lockfile
 npx pnpm@9 --filter @workspace/betpulse build
 npx pnpm@9 --filter @workspace/api-server build
 echo "Render Build Completed Successfully!"
